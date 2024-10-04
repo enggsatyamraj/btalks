cac1ff9beb530d2da30b1392fb4cdf0129e62abc
enggsatyamraj <sattu62025@gmail.com>
2024-10-05 01:11:15 +0530

    initial commit

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,9 +1,18 @@
-import { log } from "@repo/logger";
-import { createServer } from "./server";
+import express from 'express';
+import { sendResponse } from './controllers/auth-controller';
 
-const port = process.env.PORT || 5001;
-const server = createServer();
+const app = express();
 
-server.listen(port, () => {
-  log(`api running on ${port}`);
-});
+app.get("/",(req,res)=>{
+    res.status(400).json({
+        success:true,
+        message:"Server is running",
+    })
+})
+
+app.get("/send",sendResponse);
+
+
+app.listen(4000,()=>{
+    console.log("server is running in the port ", 4000)
+})
